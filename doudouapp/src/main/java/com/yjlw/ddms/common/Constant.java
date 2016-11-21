06aed102db4bd87d58287fda67a7f4ed2884b41c
wen <wenju7816673>
2016-11-21 17:41:46 +0800

    数据下载

--- doudouapp/src/main/java/com/yjlw/ddms/common/Constant.java
+++ doudouapp/src/main/java/com/yjlw/ddms/common/Constant.java
@@ -36,10 +36,7 @@ public class Constant {
     /**
      * 热门菜谱——全部
      */
-    public static String SECOND_PAGE_ALLPOPULARRECIPES = "http://api.haodou.com/index.php?appid=2&appkey=9ef269eec4f7a9d07c73952d06b5413f&format=json&sessionid=" +
-            "1479692503348&vc=105&vn=6.1.15&loguid=0&deviceid=haodou864394010609719&uuid=4d026196b079f72c6ee96157c0c65d62" +
-            "&channel=anzhi_v6115&method=Recipe.getCollectRecomment&virtual=&signmethod=md5&v=3&timestamp=" +
-            "1479714957&nonce=0.8636796165597657&appsign=5c53108e4ef75dd09e492a8efc1cef98";
+    public static String SECOND_PAGE_ALLPOPULARRECIPES = "http://api.haodou.com/index.php?appid=2&appkey=9ef269eec4f7a9d07c73952d06b5413f&format=json&sessionid=1479711964067&vc=105&vn=6.1.15&loguid=10283273&deviceid=haodou864394010248476&uuid=bdf542fa8eb3bcbc457a5383dc48fc0c&channel=anzhi_v6115&method=Recipe.getCollectRecomment&virtual=&signmethod=md5&v=3&timestamp=1479714919&nonce=0.894336285475347&appsign=536956210180d7125b7ff81e68e7821b";
 
     /*
     params.addBodyParameter("limit", "10");
