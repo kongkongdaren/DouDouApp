441c186fdd95484c986ec91b33338db3c04c9e81
wen <wenju7816673>
2016-11-21 15:29:50 +0800

    检验

---
+++ doudouapp/src/main/java/com/yjlw/ddms/fristentity/entity/HotAll.java
@@ -0,0 +1,14 @@
+package com.yjlw.ddms.fristentity.entity;
+
+/**
+ * Description：xx <br/>
+ * Copyright (c) 2016<br/>
+ * This program is protected by copyright laws <br/>
+ * Date: 2016年11月21  15:05
+ *
+ * @author 姜文莒
+ * @version : 1.0
+ */
+
+public class HotAll {
+}
