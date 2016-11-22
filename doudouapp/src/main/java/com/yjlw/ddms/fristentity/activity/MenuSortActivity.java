ef8dfab271ce8b1bb20ed93fc6398084384e2b2c
wen <wenju7816673>
2016-11-22 17:20:15 +0800

    菜谱分类

---
+++ doudouapp/src/main/java/com/yjlw/ddms/fristentity/activity/MenuSortActivity.java
@@ -0,0 +1,16 @@
+package com.yjlw.ddms.fristentity.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Description：菜谱分类 <br/>
+ * Copyright (c) 2016<br/>
+ * This program is protected by copyright laws <br/>
+ * Date: 2016年11月22  17:09
+ *
+ * @author 姜文莒
+ * @version : 1.0
+ */
+
+public class MenuSortActivity extends AppCompatActivity {
+}
