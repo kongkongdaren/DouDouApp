ae97348c32c1436e69d9c3c25283af82666f3d02
JoinXiaoHai <506731cjl>
2016-11-25 14:13:49 +0800

    特价趁现在

--- doudouapp/src/main/java/com/yjlw/ddms/homeentity/activity/EveryBargainPriceActivity.java
+++ doudouapp/src/main/java/com/yjlw/ddms/homeentity/activity/EveryBargainPriceActivity.java
@@ -2,12 +2,22 @@ package com.yjlw.ddms.homeentity.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
 import com.yjlw.ddms.R;
+import com.yjlw.ddms.common.Constant;
+import com.yjlw.ddms.homeentity.entity.LabelDetailsResult;
+import com.yjlw.ddms.homeentity.entity.SalePriceBean;
 
+import org.xutils.common.Callback;
+import org.xutils.http.HttpMethod;
+import org.xutils.http.RequestParams;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import static android.R.id.list;
 
 /**
  * 特价趁现在
@@ -21,7 +31,49 @@ public class EveryBargainPriceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_every_bargain_price);
         x.view().inject(this);
+        downLoadData();
         aboutListView();
+    }
+
+    /**
+     * 下载数据
+     */
+    private void downLoadData() {
+        String url = Constant.SECOND_PAGE_EVERY_MONEY;
+        RequestParams params = new RequestParams(url);
+        params.addBodyParameter("limit", "20");
+        params.addBodyParameter("offset", "0");
+        params.addBodyParameter("appqs", "haodourecipe://haodou.com/goods/subjectlist/?id=173");
+        params.addBodyParameter("Id", "173");
+        x.http().request(HttpMethod.POST, params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String s) {
+                parserSecondPageData(s);
+                Log.i("Log",s);
+            }
+
+            @Override
+            public void onError(Throwable throwable, boolean b) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException e) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+    }
+
+    private void parserSecondPageData(String s) {
+        Gson gson = new Gson();
+        SalePriceBean salePriceBean = gson.fromJson(s, SalePriceBean.class);
+        Log.i("Log",salePriceBean.toString());
+
     }
 
     /**
