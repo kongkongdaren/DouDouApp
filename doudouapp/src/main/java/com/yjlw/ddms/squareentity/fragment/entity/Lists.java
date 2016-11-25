c95e8e4e7d5520fb9a6d145b6eb21f85ab79ba44
TomCatLi <jl18209461001>
2016-11-25 19:04:18 +0800

    豆友的实体类

---
+++ doudouapp/src/main/java/com/yjlw/ddms/squareentity/fragment/entity/Lists.java
@@ -0,0 +1,200 @@
+package com.yjlw.ddms.squareentity.fragment.entity;
+
+import java.util.List;
+
+/**
+ * Description：豆友的实体类<br/>
+ * Copyright (c) 2016,JansonLi<br/>
+ * This program is protected by copyright laws<br/>
+ * Date:2016年11月25下午 7:01
+ *
+ * @author 李磊
+ * @version : 1.0
+ */
+
+public class Lists {
+
+    /**
+     * count : 100
+     * list : [{"Avatar":"http://avatar1.hoto.cn/e1/b7/7452641_185.jpg?v=4","UserName":"浩轩and馨瑶","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=7452641","Gender":0,"UserId":7452641,"Vip":1,"Intro":"儿女双全的宝妈，爱美食，享受生活，乐观。星星在哪里都是很亮的，就看你有没有抬头去看他们。","Relation":0,"Address":"中国山东省青岛市","FavoriteList":[],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar1.hoto.cn/bb/7f/1802171_185.jpg","UserName":"立青王与","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=1802171","Gender":0,"UserId":1802171,"Vip":1,"Intro":"北京豆友会亲子活动大掌柜！\n善于组织各类亲子活动，无论是美食活动，还是户外活动，她都能组织的井井有条。","Relation":0,"Address":"北京","FavoriteList":[{"Id":14,"Name":"小清新"},{"Id":17,"Name":"斋素"},{"Id":21,"Name":"家常菜"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar1.hoto.cn/89/01/6226313_185.jpg?v=1","UserName":"Rare100","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=6226313","Gender":0,"UserId":6226313,"Vip":1,"Intro":"儿童创意菜专家，烘焙达人！是一位热爱的生活的美丽辣妈！","Relation":0,"Address":"中国浙江省杭州市萧山区","FavoriteList":[{"Id":23,"Name":"西点烘焙"},{"Id":21,"Name":"家常菜"},{"Id":27,"Name":"主食"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar1.hoto.cn/91/f4/5371025_185.jpg?v=13","UserName":"MONGNA","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=5371025","Gender":2,"UserId":5371025,"Vip":1,"Intro":"小组生活盟主。热爱生活，热爱好豆，爱好旅游，喜欢做一个快乐的行摄人!","Relation":0,"Address":"广西省南宁市","FavoriteList":[],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar1.hoto.cn/9f/a7/5416863_185.jpg?v=6","UserName":"Qiuyue0815","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=5416863","Gender":0,"UserId":5416863,"Vip":1,"Intro":"家常菜专家","Relation":0,"Address":"中国河北省廊坊市","FavoriteList":[],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/82/e6/1173122_185.jpg","UserName":"轩瑞小馋猫","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=1173122","Gender":0,"UserId":1173122,"Vip":1,"Intro":"努力学习中","Relation":0,"Address":"中国陕西省渭南市","FavoriteList":[{"Id":12,"Name":"酸甜可口"},{"Id":13,"Name":"无辣不欢"},{"Id":15,"Name":"重口味"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/0e/c5/6735118_185.jpg?v=2","UserName":"wujiagege","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=6735118","Gender":0,"UserId":6735118,"Vip":1,"Intro":"热爱厨房喜欢美食，更喜欢自己动手分享美食。","Relation":0,"Address":"辽宁省营口市","FavoriteList":[{"Id":25,"Name":"面食"},{"Id":22,"Name":"特色料理"},{"Id":27,"Name":"主食"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/68/5c/5987432_185.jpg?v=1","UserName":"楊尐妮","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=5987432","Gender":0,"UserId":5987432,"Vip":1,"Intro":"美食是一种生活态度，用心经营，会发现生活也因此变得美好！","Relation":0,"Address":"辽宁省大连市","FavoriteList":[{"Id":17,"Name":"斋素"},{"Id":21,"Name":"家常菜"},{"Id":22,"Name":"特色料理"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/28/d7/2479912_185.jpg?v=7","UserName":"我爱咯蹦豆","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=2479912","Gender":0,"UserId":2479912,"Vip":1,"Intro":"我是个吃货，热爱美食，希望通过收集优质菜谱让我厨艺大增。","Relation":0,"Address":"湖北省襄阳市","FavoriteList":[{"Id":13,"Name":"无辣不欢"},{"Id":21,"Name":"家常菜"},{"Id":22,"Name":"特色料理"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/2e/7a/4291118_185.jpg","UserName":"吃货一枚Orz","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=4291118","Gender":0,"UserId":4291118,"Vip":1,"Intro":"喜爱美食，也喜欢收集大家做的美食~","Relation":0,"Address":"中国四川省遂宁市船山区","FavoriteList":[{"Id":11,"Name":"奶香浓郁"},{"Id":23,"Name":"西点烘焙"},{"Id":25,"Name":"面食"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/14/fa/4192788_185.jpg","UserName":"sminey豆豆","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=4192788","Gender":2,"UserId":4192788,"Vip":1,"Intro":"川菜达人。擅长川菜，家常菜，煲汤，面食，喜欢尝试各种美食，喜欢和大家分享生活中的一切美好。","Relation":0,"Address":"四川省成都市","FavoriteList":[],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/9c/77/6649756_185.jpg?v=8","UserName":"厨房爱好者987","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=6649756","Gender":1,"UserId":6649756,"Vip":1,"Intro":"菜谱专辑达人。。做一个真正的吃货玩家","Relation":0,"Address":"中国福建省泉州市安溪县","FavoriteList":[{"Id":16,"Name":"就爱吃肉"},{"Id":25,"Name":"面食"},{"Id":21,"Name":"家常菜"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar1.hoto.cn/fd/f3/6091773_185.jpg?v=3","UserName":"真馜花","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=6091773","Gender":0,"UserId":6091773,"Vip":1,"Intro":"喜欢看外国饮食节目，学习外国烹饪技巧和菜谱，在网上与大家分享，也曾翻译过甜品菜谱，是一位喜欢烘焙、中西餐私房菜的妈妈。","Relation":0,"Address":"广东省珠海市","FavoriteList":[{"Id":17,"Name":"斋素"},{"Id":21,"Name":"家常菜"},{"Id":22,"Name":"特色料理"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/7a/44/5325946_185.jpg?v=4","UserName":"蕾拉Reira","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=5325946","Gender":0,"UserId":5325946,"Vip":1,"Intro":"自由美食摄影师，自媒体人。喜欢烹饪和拍摄各种美食。","Relation":0,"Address":"湖北省武汉市","FavoriteList":[],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/fa/a9/9087482_185.jpg?v=2","UserName":"希葩提娅","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=9087482","Gender":0,"UserId":9087482,"Vip":1,"Intro":"烘焙专家。一位爱烘焙爱到骨子里的发烧友，喜欢甜品，更爱与人分享美食的点点滴滴。","Relation":0,"Address":"","FavoriteList":[],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar1.hoto.cn/dd/d5/2479581_185.jpg?v=19","UserName":"平淡一生yan","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=2479581","Gender":0,"UserId":2479581,"Vip":1,"Intro":"烘焙达人，家常菜达人。为爱下厨的全职煮妇。热爱美食，更爱与人分享，幸福生活的秘诀就是快乐！","Relation":0,"Address":"中国福建省泉州市","FavoriteList":[{"Id":16,"Name":"就爱吃肉"},{"Id":23,"Name":"西点烘焙"},{"Id":21,"Name":"家常菜"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar1.hoto.cn/c3/49/5982659_185.jpg?v=1","UserName":"臭美园妈","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=5982659","Gender":0,"UserId":5982659,"Vip":1,"Intro":"烘焙专家。在各种烘焙大赛中获得大奖。更是一位热爱家庭的超级辣妈！","Relation":0,"Address":"浙江省嘉兴市","FavoriteList":[{"Id":11,"Name":"奶香浓郁"},{"Id":14,"Name":"小清新"},{"Id":23,"Name":"西点烘焙"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/36/cb/7719734_185.jpg?v=6","UserName":"花花1977","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=7719734","Gender":0,"UserId":7719734,"Vip":1,"Intro":"小组生活盟主。简单生活，专心美食，热心分享。对美食，对营养知识有着一颗不断学习上进的心。","Relation":0,"Address":"中国辽宁省葫芦岛市连山区","FavoriteList":[],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/2c/62/8872492_185.jpg?v=4","UserName":"小沫虾子糕","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=8872492","Gender":0,"UserId":8872492,"Vip":1,"Intro":"擅长沪菜，本帮菜，烘焙，甜点，享受在厨房的每分每秒，乐于把美食分享给大家，喜欢不断提升自我，挑战自我，完善自我","Relation":0,"Address":"英国","FavoriteList":[{"Id":16,"Name":"就爱吃肉"},{"Id":21,"Name":"家常菜"},{"Id":22,"Name":"特色料理"}],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0},{"Avatar":"http://avatar0.hoto.cn/78/99/4299128_185.jpg?v=20","UserName":"hrb木子","OpenUrl":"haodourecipe://haodou.com/user/wo/?id=4299128","Gender":0,"UserId":4299128,"Vip":1,"Intro":"小组生活盟主。喜欢做美食，享受美食带给家人的快乐。喜欢行摄走天下，乐在其中，乐于与豆亲们一起分享行行摄摄的生活。","Relation":0,"Address":"中国黑龙江省哈尔滨市松北区","FavoriteList":[],"SameFeature":"","ListUrl":"","StyleType":3,"entityType":0}]
+     * searchCondList : [{"Title":"全部","Value":"default"},{"Title":"共同兴趣","Value":"commonInterest"},{"Title":"附近的人","Value":"distance"},{"Title":"只看女的","Value":"woman"},{"Title":"只看男的","Value":"man"}]
+     * FavoriteList : [{"Id":11,"Name":"奶香浓郁","Img":"http://www.haodou.com/public/images/user_center/array_data/11.jpg","Check":false},{"Id":12,"Name":"酸甜可口","Img":"http://www.haodou.com/public/images/user_center/array_data/12.jpg","Check":false},{"Id":13,"Name":"无辣不欢","Img":"http://www.haodou.com/public/images/user_center/array_data/13.jpg","Check":false},{"Id":14,"Name":"小清新","Img":"http://www.haodou.com/public/images/user_center/array_data/14.jpg","Check":false},{"Id":15,"Name":"重口味","Img":"http://www.haodou.com/public/images/user_center/array_data/15.jpg","Check":false},{"Id":16,"Name":"就爱吃肉","Img":"http://www.haodou.com/public/images/user_center/array_data/16.jpg","Check":false},{"Id":17,"Name":"斋素","Img":"http://www.haodou.com/public/images/user_center/array_data/17.jpg","Check":false},{"Id":23,"Name":"西点烘焙","Img":"http://www.haodou.com/public/images/user_center/array_data/23.jpg","Check":false},{"Id":25,"Name":"面食","Img":"http://www.haodou.com/public/images/user_center/array_data/25.jpg","Check":false},{"Id":32,"Name":"美容","Img":"http://www.haodou.com/public/images/user_center/array_data/32.jpg","Check":false},{"Id":41,"Name":"家有宝宝","Img":"http://www.haodou.com/public/images/user_center/array_data/41.jpg","Check":false},{"Id":44,"Name":"孕妇","Img":"http://www.haodou.com/public/images/user_center/array_data/44.jpg","Check":false},{"Id":21,"Name":"家常菜","Img":"http://www.haodou.com/public/images/user_center/array_data/21.jpg","Check":false},{"Id":22,"Name":"特色料理","Img":"http://www.haodou.com/public/images/user_center/array_data/22.jpg","Check":false},{"Id":24,"Name":"凉菜","Img":"http://www.haodou.com/public/images/user_center/array_data/24.jpg","Check":false},{"Id":26,"Name":"饮品","Img":"http://www.haodou.com/public/images/user_center/array_data/26.jpg","Check":false},{"Id":27,"Name":"主食","Img":"http://www.haodou.com/public/images/user_center/array_data/27.jpg","Check":false},{"Id":31,"Name":"减肥","Img":"http://www.haodou.com/public/images/user_center/array_data/31.jpg","Check":false},{"Id":33,"Name":"补钙","Img":"http://www.haodou.com/public/images/user_center/array_data/33.jpg","Check":false},{"Id":34,"Name":"清热去火","Img":"http://www.haodou.com/public/images/user_center/array_data/34.jpg","Check":false},{"Id":35,"Name":"健脾开胃","Img":"http://www.haodou.com/public/images/user_center/array_data/35.jpg","Check":false},{"Id":36,"Name":"提高免疫力","Img":"http://www.haodou.com/public/images/user_center/array_data/36.jpg","Check":false},{"Id":37,"Name":"防辐射","Img":"http://www.haodou.com/public/images/user_center/array_data/37.jpg","Check":false},{"Id":42,"Name":"上班族","Img":"http://www.haodou.com/public/images/user_center/array_data/42.jpg","Check":false},{"Id":43,"Name":"青少年成长","Img":"http://www.haodou.com/public/images/user_center/array_data/43.jpg","Check":false},{"Id":45,"Name":"老年人","Img":"http://www.haodou.com/public/images/user_center/array_data/45.jpg","Check":false},{"Id":46,"Name":"快手菜","Img":"http://www.haodou.com/public/images/user_center/array_data/46.jpg","Check":false}]
+     */
+
+    private ResultBean result;
+
+    public ResultBean getResult() {
+        return result;
+    }
+
+    public void setResult(ResultBean result) {
+        this.result = result;
+    }
+
+    public static class ResultBean {
+        private int count;
+        /**
+         * Avatar : http://avatar1.hoto.cn/e1/b7/7452641_185.jpg?v=4
+         * UserName : 浩轩and馨瑶
+         * OpenUrl : haodourecipe://haodou.com/user/wo/?id=7452641
+         * Gender : 0
+         * UserId : 7452641
+         * Vip : 1
+         * Intro : 儿女双全的宝妈，爱美食，享受生活，乐观。星星在哪里都是很亮的，就看你有没有抬头去看他们。
+         * Relation : 0
+         * Address : 中国山东省青岛市
+         * FavoriteList : []
+         * SameFeature :
+         * ListUrl :
+         * StyleType : 3
+         * entityType : 0
+         */
+
+        private List<ListBean> list;
+
+        public int getCount() {
+            return count;
+        }
+
+        public void setCount(int count) {
+            this.count = count;
+        }
+
+        public List<ListBean> getList() {
+            return list;
+        }
+
+        public void setList(List<ListBean> list) {
+            this.list = list;
+        }
+
+        public static class ListBean {
+            private String Avatar;
+            private String UserName;
+            private String OpenUrl;
+            private int Gender;
+            private int UserId;
+            private int Vip;
+            private String Intro;
+            private int Relation;
+            private String Address;
+            private String SameFeature;
+            private String ListUrl;
+            private int StyleType;
+            private int entityType;
+            private List<?> FavoriteList;
+
+            public String getAvatar() {
+                return Avatar;
+            }
+
+            public void setAvatar(String Avatar) {
+                this.Avatar = Avatar;
+            }
+
+            public String getUserName() {
+                return UserName;
+            }
+
+            public void setUserName(String UserName) {
+                this.UserName = UserName;
+            }
+
+            public String getOpenUrl() {
+                return OpenUrl;
+            }
+
+            public void setOpenUrl(String OpenUrl) {
+                this.OpenUrl = OpenUrl;
+            }
+
+            public int getGender() {
+                return Gender;
+            }
+
+            public void setGender(int Gender) {
+                this.Gender = Gender;
+            }
+
+            public int getUserId() {
+                return UserId;
+            }
+
+            public void setUserId(int UserId) {
+                this.UserId = UserId;
+            }
+
+            public int getVip() {
+                return Vip;
+            }
+
+            public void setVip(int Vip) {
+                this.Vip = Vip;
+            }
+
+            public String getIntro() {
+                return Intro;
+            }
+
+            public void setIntro(String Intro) {
+                this.Intro = Intro;
+            }
+
+            public int getRelation() {
+                return Relation;
+            }
+
+            public void setRelation(int Relation) {
+                this.Relation = Relation;
+            }
+
+            public String getAddress() {
+                return Address;
+            }
+
+            public void setAddress(String Address) {
+                this.Address = Address;
+            }
+
+            public String getSameFeature() {
+                return SameFeature;
+            }
+
+            public void setSameFeature(String SameFeature) {
+                this.SameFeature = SameFeature;
+            }
+
+            public String getListUrl() {
+                return ListUrl;
+            }
+
+            public void setListUrl(String ListUrl) {
+                this.ListUrl = ListUrl;
+            }
+
+            public int getStyleType() {
+                return StyleType;
+            }
+
+            public void setStyleType(int StyleType) {
+                this.StyleType = StyleType;
+            }
+
+            public int getEntityType() {
+                return entityType;
+            }
+
+            public void setEntityType(int entityType) {
+                this.entityType = entityType;
+            }
+
+            public List<?> getFavoriteList() {
+                return FavoriteList;
+            }
+
+            public void setFavoriteList(List<?> FavoriteList) {
+                this.FavoriteList = FavoriteList;
+            }
+        }
+    }
+}
