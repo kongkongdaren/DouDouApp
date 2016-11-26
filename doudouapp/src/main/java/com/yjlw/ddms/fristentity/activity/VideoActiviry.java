e2328ebc5f043fac1701ab138b6188f1b53df6fc
wen <wenju7816673>
2016-11-26 14:51:41 +0800

    Vitamia

---
+++ doudouapp/src/main/java/com/yjlw/ddms/fristentity/activity/VideoActiviry.java
@@ -0,0 +1,17 @@
+package com.yjlw.ddms.fristentity.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Description：xx <br/>
+ * Copyright (c) 2016<br/>
+ * This program is protected by copyright laws <br/>
+ * Date: 2016年11月26  13:52
+ *
+ * @author 姜文莒
+ * @version : 1.0
+ */
+
+public class VideoActiviry extends AppCompatActivity {
+
+}
