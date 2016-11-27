21b35c6d51e704b0e9ac19e49cdf11033b39941b
wen <wenju7816673>
2016-11-27 19:22:06 +0800

    刷新实现

--- doudouapp/src/main/java/com/yjlw/ddms/fristentity/activity/HotActivity.java
+++ doudouapp/src/main/java/com/yjlw/ddms/fristentity/activity/HotActivity.java
@@ -3,12 +3,15 @@ package com.yjlw.ddms.fristentity.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import com.google.gson.Gson;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.yjlw.ddms.R;
 import com.yjlw.ddms.common.Constant;
 import com.yjlw.ddms.fristentity.adapter.HotActivityAdapter;
@@ -36,9 +39,11 @@ public class HotActivity extends AppCompatActivity {
     @ViewInject(R.id.iv_back)
     private ImageView ivBack;
     @ViewInject(R.id.lv_hot_activity)
-    private ListView lv;
+    private PullToRefreshListView lv;
     @ViewInject(R.id.pb_hot)
     private ProgressBar pb;
+    private HotActivityAdapter adapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -106,9 +111,67 @@ public class HotActivity extends AppCompatActivity {
      * 关于listview 的操作
      * @param hotList
      */
-    private void aboutListView(List<HotAndData.ResultBean.ListBean> hotList) {
+    private void aboutListView(final List<HotAndData.ResultBean.ListBean> hotList) {
         //适配器
-        HotActivityAdapter adapter=new HotActivityAdapter(hotList,this);
+        adapter = new HotActivityAdapter(hotList,this);
         lv.setAdapter(adapter);
+        //设置PullRefreshListView上提加载时的加载提示
+        lv.setMode(PullToRefreshBase.Mode.BOTH);
+        String str = DateUtils.formatDateTime(HotActivity.this, System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+        lv.getLoadingLayoutProxy(false, true).setPullLabel("上拉加载...");
+        lv.getLoadingLayoutProxy(false, true).setRefreshingLabel("正在加载...");
+        lv.getLoadingLayoutProxy(false, true).setReleaseLabel("松开加载更多...");
+        //设置上一次刷新的提示标签
+        lv.getLoadingLayoutProxy().setLastUpdatedLabel("最后更新时间:" + str);
+        // 设置PullRefreshListView下拉加载时的加载提示
+        lv.getLoadingLayoutProxy(true, false).setPullLabel("下拉刷新...");
+        lv.getLoadingLayoutProxy(true, false).setRefreshingLabel("正在刷新...");
+        lv.getLoadingLayoutProxy(true, false).setReleaseLabel("松开刷新...");
+
+        lv.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
+            @Override
+            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+                refreshData(hotList);
+
+            }
+
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+                refreshData(hotList);
+            }
+        });
+    }
+    private void refreshData(final List<HotAndData.ResultBean.ListBean> hotList) {
+        String hotUrl = Constant.ONE_PAGE_POPLAR_ACTIVITIES;
+        RequestParams params=new RequestParams(hotUrl);
+        params.addBodyParameter("limit", "20");
+        params.addBodyParameter("offset", "0");
+        params.addBodyParameter("appqs", "haodourecipe://haodou.com/latestactive/official/");
+        x.http().request(HttpMethod.POST, params, new Callback.CommonCallback<String> () {
+            @Override
+            public void onSuccess(String result) {
+                Gson gson=new Gson();
+                HotAndData hotAndActivity = gson.fromJson(result, HotAndData.class);
+                List<HotAndData.ResultBean.ListBean> hotRefreshList = hotAndActivity.getResult().getList();
+                hotList.addAll(hotRefreshList);
+                adapter.notifyDataSetChanged();
+                lv.onRefreshComplete();
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
     }
 }
