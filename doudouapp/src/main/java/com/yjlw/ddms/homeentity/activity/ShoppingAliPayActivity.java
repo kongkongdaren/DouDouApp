fc495fc3da4afb800bf541da4a74781190ed3051
JoinXiaoHai <506731cjl>
2016-11-28 15:19:16 +0800

    支付宝支付jar提交

---
+++ doudouapp/src/main/java/com/yjlw/ddms/homeentity/activity/ShoppingAliPayActivity.java
@@ -0,0 +1,18 @@
+package com.yjlw.ddms.homeentity.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yjlw.ddms.R;
+
+/**
+ * 阿里支付
+ */
+public class ShoppingAliPayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_ali_pay);
+    }
+}
