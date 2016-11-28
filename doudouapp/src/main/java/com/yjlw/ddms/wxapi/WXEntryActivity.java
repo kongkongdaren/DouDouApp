6df75cdf4bca3453f2ac879e49c56aa56a9918a5
wen <wenju7816673>
2016-11-28 22:01:46 +0800

    U盟分享

---
+++ doudouapp/src/main/java/com/yjlw/ddms/wxapi/WXEntryActivity.java
@@ -0,0 +1,16 @@
+package com.yjlw.ddms.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * Description：xx <br/>
+ * Copyright (c) 2016<br/>
+ * This program is protected by copyright laws <br/>
+ * Date: 2016年11月28  21:24
+ *
+ * @author 姜文莒
+ * @version : 1.0
+ */
+
+public class WXEntryActivity extends WXCallbackActivity {
+}
