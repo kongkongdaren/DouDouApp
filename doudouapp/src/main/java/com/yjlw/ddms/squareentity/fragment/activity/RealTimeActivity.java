bc3d99c7415feee981ff93dbc233b7cc0de9e9a0
TomCatLi <jl18209461001>
2016-11-29 23:22:38 +0800

    第三页数据加载

---
+++ doudouapp/src/main/java/com/yjlw/ddms/squareentity/fragment/activity/RealTimeActivity.java
@@ -0,0 +1,25 @@
+package com.yjlw.ddms.squareentity.fragment.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.yjlw.ddms.R;
+
+/**
+ * Description：实时热点的跳转页面<br/>
+ * Copyright (c) 2016,JansonLi<br/>
+ * This program is protected by copyright laws<br/>
+ * Date:2016年11月29下午 10:47
+ *
+ * @author 李磊
+ * @version : 1.0
+ */
+
+public class RealTimeActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_squareen_real_time);
+    }
+}
