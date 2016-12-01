3be3ddd9bdb94c6221fb437155521eff669c0107
wen <wenju7816673>
2016-12-01 14:07:46 +0800

    代码优化

---
+++ doudouapp/src/main/java/com/yjlw/ddms/fristentity/activity/ViewPagerWebActivity.java
@@ -0,0 +1,83 @@
+package com.yjlw.ddms.fristentity.activity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+
+import com.yjlw.ddms.R;
+
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+/**
+ * Description：xx <br/>
+ * Copyright (c) 2016<br/>
+ * This program is protected by copyright laws <br/>
+ * Date: 2016年11月30  20:24
+ *
+ * @author 姜文莒
+ * @version : 1.0
+ */
+
+public class ViewPagerWebActivity extends AppCompatActivity {
+    @ViewInject(R.id.caipu_back)
+    private ImageView caiBack;
+    @ViewInject(R.id.caipu_share)
+    private ImageView caiShare;
+    @ViewInject(R.id.cai_web)
+    private WebView web;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.caipu_item);
+        x.view().inject(this);
+        caiBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        Bundle extras = getIntent().getExtras();
+        String url = extras.getString("url");
+        WebSettings settings = web.getSettings();
+        settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        settings.setSupportMultipleWindows(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setSavePassword(false);
+        //设置缓存模式
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setMinimumFontSize(settings.getMinimumFontSize() + 8);
+        settings.setAllowFileAccess(false);
+        settings.setTextSize(WebSettings.TextSize.NORMAL);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        web.setWebChromeClient(new webChromeClient());// 支持运行特殊的javascript（例如：alert()）
+        web.setWebViewClient(new  webViewClient());//
+        web.loadUrl(url);
+    }
+
+    private class webChromeClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            // TODO Auto-generated method stub
+            super.onProgressChanged(view, newProgress);
+        }
+    }
+
+    private class webViewClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+    }
+}
--- doudouapp/src/main/java/com/yjlw/ddms/fristentity/fragment/ViewPagerFragment.java
+++ doudouapp/src/main/java/com/yjlw/ddms/fristentity/fragment/ViewPagerFragment.java
@@ -1,14 +1,17 @@
 package com.yjlw.ddms.fristentity.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.yjlw.ddms.R;
+import com.yjlw.ddms.fristentity.activity.ViewPagerWebActivity;
 
 import it.sephiroth.android.library.picasso.Picasso;
 
@@ -28,6 +31,7 @@ public class ViewPagerFragment extends Fragment {
     private TextView tvAdverTitle;
     private String title;
     private String img;
+    private String url;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +39,7 @@ public class ViewPagerFragment extends Fragment {
         Bundle arguments = getArguments();
         title = arguments.getString("title");
          img = arguments.getString("img");
+         url = arguments.getString("url");
         super.onCreate(savedInstanceState);
     }
 
@@ -44,6 +49,42 @@ public class ViewPagerFragment extends Fragment {
         View view = inflater.inflate(R.layout.bean_advertise_item, container, false);
         ivPhoto = (ImageView) view.findViewById(R.id.iv_photo_id);
         tvAdverTitle = (TextView) view.findViewById(R.id.tv_advertise_title);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url2 = null;
+
+                String url3 =null;
+
+                if(url.contains("collect")){
+
+                    url2 = url.substring(url.lastIndexOf("id=")+3);
+
+                    url3 = "http://www.haodou.com/recipe/album /"+url2+"/";
+
+
+                }else if(url.contains("opentopic")){
+
+                    url2 = url.substring(url.lastIndexOf("topic"),url.lastIndexOf("&"));
+
+
+                    url3 = "http://group.haodou.com /"+url2;
+                    Log.i("i",url3);
+
+                }else{
+                    url2 = url.substring(url.lastIndexOf("id=")+3,url.lastIndexOf("&"));
+
+                    url3 = "http://www.haodou.com/recipe /"+url2+"/";
+                }
+
+                Intent intent = new Intent(getActivity(),ViewPagerWebActivity.class);
+                Bundle bundle=new Bundle();
+                bundle.putString("url",url3);
+                intent.putExtras(bundle);
+                startActivity(intent);
+
+            }
+        });
         return view;
     }
     @Override
@@ -52,4 +93,5 @@ public class ViewPagerFragment extends Fragment {
         Picasso.with(getContext()).load(img).placeholder(R.mipmap.default_high).into(ivPhoto);
         super.onActivityCreated(savedInstanceState);
     }
+
 }
